fa8bd43850346474ff2b8f07a33a0dd139706134
Axel <sebarian67@gmail.com>
2026-06-05 12:00:32 -0600

    Panel viusal, le falta letra para salir y mejorar la paleta de colores

--- Hormiga_logica.py
+++ Hormiga_logica.py
@@ -1,4 +1,4 @@
-def generar_ramatriz(filas, columnas):
+def generar_matriz(filas, columnas):
     """ matriz puros 0s"""
 
     return [[0 for c in range (columnas)] for f in range(filas)]
@@ -12,41 +12,34 @@ def girar_hormiga(orientacion_actual, giro):
         return giro_derecha[orientacion_actual]
     else:
         return giro_izquierda[orientacion_actual]
-
         
-def avanzar_hormiga():
-    global hormiga_fila, hormiga_columna
-
-    if orientacion_actual == 0:
-        hormiga_fila = (hormiga_fila - 1)% filas
-    elif orientacion_actual == 1:
-        hormiga_columna = (hormiga_columna +1)%filas
-    elif orientacion_actual == 2:
-        hormiga_fila = (hormiga_fila +1) %fila
-    elif orientacion_actual == 3:
-        hormiga_columna = (hormiga_columna - 1) % columnas
 def avanzar_hormiga(filaHormiga, columnaHormiga, direccion, total_filas, total_columnas):
     
     if direccion == "R":
         return filaHormiga, (columnaHormiga + 1) % total_columnas
-    
     if direccion == "L":
         return filaHormiga, (columnaHormiga - 1) % total_columnas
-    
     if direccion == "U":
         return (filaHormiga - 1) % total_filas, columnaHormiga
-    
     if direccion == "D":
         return (filaHormiga + 1) % total_filas, columnaHormiga
 
 def transicion(M, filaHormiga, columnaHormiga, regla):
    
     total_colores = len(regla)
-    
     color_actual = M[filaHormiga][columnaHormiga]
-    
     siguiente_color = (color_actual + 1) % total_colores
-    
     M[filaHormiga][columnaHormiga] = siguiente_color
     
     return M
+def siguiente(M, filaHormiga, columnaHormiga, direccion, regla):
+    
+    color_actual = M[filaHormiga][columnaHormiga]
+    giro = regla[color_actual]
+    direccion = girar_hormiga(direccion, giro)
+    M = transicion(M, filaHormiga, columnaHormiga, regla)
+    total_filas = len(M)
+    total_columnas = len(M[0])
+    filaHormiga, columnaHormiga = avanzar_hormiga(filaHormiga, columnaHormiga, direccion, total_filas, total_columnas)
+    
+    return M, filaHormiga, columnaHormiga, direccion
---
+++ Hormiga_visual.py
@@ -0,0 +1,92 @@
+import pygame
+import random
+import sys
+import Hormiga_logica as hor
+tam = 8
+filas = 100
+columnas = 100
+tick = 1000
+print("Bienvenido a el programa de la Hormiguita de Langton")
+regla = input("Ingrese la secuencia de giros (ej. RL, LRRLL, RLR): ").strip().upper()
+while not regla or any(letra not in "LR" for letra in regla):
+    print("Error: La regla solo puede contener letras 'L' y 'R'.")
+    regla = input("Intente de nuevo: ").strip().upper()
+
+
+def generar_colores(cantidad_colores):
+    colores_base = [
+        (25, 40, 30),   
+        (255, 255, 0),    
+        (255, 0, 192)    
+    ]
+    colores = []
+    for i in range(cantidad_colores):
+        if i < len(colores_base):
+            colores.append(colores_base[i])
+        else:
+            r = random.choice([100, 255])
+            g = random.choice([100, 255])
+            b = random.choice([100, 255])
+            colores.append((r, g, b))
+            
+    return colores
+paleta_colores = generar_colores(len(regla))
+
+def main():
+    filaHormiga = filas // 2
+    columnaHormiga = columnas // 2
+    
+    direccion = "U" 
+    
+    pygame.init()
+    clock = pygame.time.Clock()
+    M = hor.generar_matriz(filas, columnas)
+    w, h = columnas * tam, filas * tam
+    window = pygame.display.set_mode((w, h))
+    pygame.display.set_caption("Hormiga de Langton Generalizada")
+    
+    loop = True
+    pausa = False
+    
+    while loop:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                loop = False
+            if event.type == pygame.KEYDOWN:
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_SPACE]:
+                    pausa = not pausa
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                buttons = pygame.mouse.get_pressed()
+                x, y = pygame.mouse.get_pos()
+                if buttons[0]:
+                    f = y // tam
+                    c = x // tam
+                    M[f][c] = (M[f][c] + 1) % len(regla)
+                    
+        window.fill((0, 0, 0))
+        
+        for f in range(filas):
+            for c in range(columnas):
+                color_estado = M[f][c]
+                color_rgb = paleta_colores[color_estado]
+                
+                if color_estado != 0:
+                    pos_x = c * tam
+                    pos_y = f * tam
+                    pygame.draw.rect(window, color_rgb, (pos_x, pos_y, tam, tam))
+        x_hormiga = columnaHormiga * tam
+        y_hormiga = filaHormiga * tam
+        pygame.draw.rect(window, (255, 0, 0), (x_hormiga, y_hormiga, tam, tam))
+                    
+        if not pausa:
+            M, filaHormiga, columnaHormiga, direccion = hor.siguiente(M, filaHormiga, columnaHormiga, direccion, regla)
+            
+        pygame.display.update()
+        clock.tick(tick)
+        
+    pygame.quit()
+    sys.exit()
+
+if __name__ == "__main__":
+    main()
